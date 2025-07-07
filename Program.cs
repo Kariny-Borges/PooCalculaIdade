16195678b4872ab32d8b941a58b669766fdf7daf
unknown <kaborges0203@gmail.com>
2025-07-07 00:02:02 -0300

    Mesclando projeto local com repositório remoto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using pooCalculaIdade;
+
+Console.WriteLine("Programa que calcula a idade de uma pessoa!");
+Pessoa p = new Pessoa();
+Console.WriteLine("Nome da pessoa:");
+p.Nome = Console.ReadLine();
+Console.WriteLine("Ano de nascimento:");
+p.AnoNascimento = Convert.ToInt32 (Console.ReadLine());
+p.ExibirDados();
+Console.ReadKey();
